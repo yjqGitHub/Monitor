ca1fd888dc2cd644ab448a09f82aa491230fcefe
叶俊权 <425527169@qq.com>
2017-06-17 17:40:47 +0800

    Mongodb

--- JQ.MongoDb/MongoBaseRepository.cs
+++ JQ.MongoDb/MongoBaseRepository.cs
@@ -43,52 +43,110 @@ namespace JQ.MongoDb
             }
         }
 
-        public TEntity InsertOne(TEntity entity)
+        public virtual TEntity InsertOne(TEntity entity)
         {
             Collection.InsertOne(entity);
             return entity;
         }
 
-        public async Task<TEntity> InsertOneAsync(TEntity entity)
+        public virtual async Task<TEntity> InsertOneAsync(TEntity entity)
         {
             await Collection.InsertOneAsync(entity);
             return entity;
         }
 
-        public void InsertMany(IEnumerable<TEntity> entityList)
+        public virtual void InsertMany(IEnumerable<TEntity> entityList)
         {
             Collection.InsertMany(entityList);
         }
 
-        public Task InsertManyAsync(IEnumerable<TEntity> entityList)
+        public virtual Task InsertManyAsync(IEnumerable<TEntity> entityList)
         {
             return Collection.InsertManyAsync(entityList);
         }
 
 
-        public bool DeleteOne(Expression<Func<TEntity, bool>> filter)
+        public virtual bool DeleteOne(Expression<Func<TEntity, bool>> filter)
         {
             var deleteResult = Collection.DeleteOne(filter);
             return deleteResult.DeletedCount > 0;
         }
 
-        public async Task<bool> DeleteOneAsync(Expression<Func<TEntity, bool>> filter)
+        public virtual async Task<bool> DeleteOneAsync(Expression<Func<TEntity, bool>> filter)
         {
             var deleteResult = await Collection.DeleteOneAsync(filter);
             return deleteResult.DeletedCount > 0;
         }
 
-        public long DeleteMany(Expression<Func<TEntity, bool>> filter)
+        public virtual long DeleteMany(Expression<Func<TEntity, bool>> filter)
         {
             var deleteResult = Collection.DeleteMany(filter);
             return deleteResult.DeletedCount;
         }
 
-        public async Task<long> DeleteManyAsync(Expression<Func<TEntity, bool>> filter)
+        public virtual async Task<long> DeleteManyAsync(Expression<Func<TEntity, bool>> filter)
         {
             var deleteResult = await Collection.DeleteManyAsync(filter);
             return deleteResult.DeletedCount;
         }
         //public bool UpdateOne(Expression<Func<TEntity, bool>> filter,)
+
+        public virtual TEntity GetInfo(Expression<Func<TEntity, bool>> filter)
+        {
+            return Collection.Find(filter).FirstOrDefault();
+        }
+
+        public virtual Task<TEntity> GetInfoAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return Collection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public virtual IQueryable<TEntity> GetAll()
+        {
+            return Collection.AsQueryable();
+        }
+
+        public virtual IEnumerable<TEntity> GetList(Expression<Func<TEntity, bool>> filter)
+        {
+            return Collection.Find(filter).ToEnumerable();
+        }
+
+        public virtual Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return Collection.FindAsync(filter).ContinueWith((m) => { return m.Result.ToEnumerable(); });
+        }
+
+        public virtual long Count()
+        {
+            return GetAll().Count();
+        }
+
+        public virtual Task<long> CountAsync()
+        {
+            return Task.FromResult(Count());
+        }
+
+        public virtual long Count(Expression<Func<TEntity, bool>> filter)
+        {
+            return Collection.Count(filter);
+        }
+
+        public virtual Task<long> CountAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return Collection.CountAsync(filter);
+        }
+
+        public virtual IEnumerable<TEntity> PageQuery(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, object>> sort, int pageIndex, int pageSize, bool isDesc)
+        {
+            long totalCount = Count(filter);
+            if (isDesc)
+            {
+                return Collection.Find(filter).SortByDescending(sort).Skip((pageIndex - 1) * pageSize).Limit(pageSize).ToEnumerable();
+            }
+            else
+            {
+                return Collection.Find(filter).SortBy(sort).Skip((pageIndex - 1) * pageSize).Limit(pageSize).ToEnumerable();
+            }
+        }
     }
 }
